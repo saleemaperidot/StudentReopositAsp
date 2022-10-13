a9faa50a57f63aa92a061e37eedcd2ad54355e0c
saleema np <saleemaperidot@gmail.com>
2022-10-13 23:11:17 +0530

    initial commit

---
+++ StudentData/Models/StudentModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentData.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? StudentUserName { get; set; }
+        [Required]
+        public string? StudentPassword { get; set; }
+        [Required]
+        public string? FathersName { get; set; }
+        [Required]
+        public string? StudentAddress { get; set; }
+        [Required]
+        public string? City { get; set; }
+        [Required]
+        public string? ContactNumber { get; set; }
+        [Required]
+        public DateTime DateOfbirth { get; set; }
+        [Required]
+        public DateTime DateOfJoining { get; set; }
+
+
+
+
+
+        
+
+    }
+}
